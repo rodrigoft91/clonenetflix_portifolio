505c6cf6414253bb4d89e05c63ef26989c6e9c73
rodrigoft91 <107896164+rodrigoft91@users.noreply.github.com>
2022-12-23 16:34:16 -0300

    pagina responsiva

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+const botaoMenu = document.querySelector('.botao__navegacao')
+const menu = document.querySelector('.menu-lateral')
+
+    botaoMenu.addEventListener('click', () =>{
+
+        menu.classList.toggle('menu-lateral--ativo')
+
+
+    })
